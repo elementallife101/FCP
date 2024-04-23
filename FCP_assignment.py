35d5335ab2b7e5269c6178a55c1cc70297983f9e
Rory Sutherden <rory.sutherden@gmail.com>
2024-04-23 13:07:55 +0100

    updated FCP_assignment.py to v3

--- FCP_assignment.py
+++ FCP_assignment.py
@@ -8,34 +8,24 @@ class Node:
 		self.connections = connections
 		self.value = value
 
-class Network:
+class Network: 
+
 	def __init__(self, nodes=None):
 		if nodes is None:
 			self.nodes = []
 		else:
 			self.nodes = nodes 
 
-	"""
-	COMPLETE - NEEDS FURTHER DRAFTING TO IMPROVE READABILITY
-	"""
 	def get_mean_degree(self):
-		print("")
-		list_of_lengths = []
-		for examplenode in self.nodes:
-				print(examplenode.connections)
-				print(str(len(examplenode.connections))+"\n")
-				list_of_lengths.append(len(examplenode.connections))
-		return mean(list_of_lengths)
+		#Your code  for task 3 goes here
 
 	def get_mean_clustering(self):
-			print("")
 		#Your code for task 3 goes here
 
 	def get_mean_path_length(self):
-			print("")
 		#Your code for task 3 goes here
 
-	def make_random_network(self, N, connection_probability):
+	def make_random_network(self, N, connection_probability=0.5):
 		'''
 		This function makes a *random* network of size N.
 		Each node is connected to each other node with probability p
@@ -52,24 +42,24 @@ class Network:
 				if np.random.random() < connection_probability:
 					node.connections[neighbour_index] = 1
 					self.nodes[neighbour_index].connections[index] = 1
-		
 
 	def make_ring_network(self, N, neighbour_range=1):
-			print("")
 		#Your code  for task 4 goes here
 
 	def make_small_world_network(self, N, re_wire_prob=0.2):
-			print("")
 		#Your code for task 4 goes here
 
 	def plot(self):
+
 		fig = plt.figure()
 		ax = fig.add_subplot(111)
 		ax.set_axis_off()
+
 		num_nodes = len(self.nodes)
 		network_radius = num_nodes * 10
 		ax.set_xlim([-1.1*network_radius, 1.1*network_radius])
 		ax.set_ylim([-1.1*network_radius, 1.1*network_radius])
+
 		for (i, node) in enumerate(self.nodes):
 			node_angle = i * 2 * np.pi / num_nodes
 			node_x = network_radius * np.cos(node_angle)
@@ -77,11 +67,13 @@ class Network:
 
 			circle = plt.Circle((node_x, node_y), 0.3*num_nodes, color=cm.hot(node.value))
 			ax.add_patch(circle)
+
 			for neighbour_index in range(i+1, num_nodes):
 				if node.connections[neighbour_index]:
 					neighbour_angle = neighbour_index * 2 * np.pi / num_nodes
 					neighbour_x = network_radius * np.cos(neighbour_angle)
 					neighbour_y = network_radius * np.sin(neighbour_angle)
+
 					ax.plot((node_x, neighbour_x), (node_y, neighbour_y), color='black')
 
 def test_networks():
@@ -140,7 +132,7 @@ This section contains code for the Ising Model - task 1 in the assignment
 
 def calculate_agreement(population, row, col, external=0.0):
 	'''
-	This function should return the *change* in agreement that would result if the cell at (row, col) was to flip it's value
+	This function should return the extent to which a cell agrees with its neighbours.
 	Inputs: population (numpy array)
 			row (int)
 			col (int)
@@ -151,7 +143,7 @@ def calculate_agreement(population, row, col, external=0.0):
 
 	#Your code for task 1 goes here
 
-	return np.random * population
+	return np.random.random() * population
 
 def ising_step(population, external=0.0):
 	'''
@@ -175,9 +167,10 @@ def plot_ising(im, population):
 	'''
 	This function will display a plot of the Ising model
 	'''
-	new_im = np.array([[255 if val == -1 else 1 for val in rows] for rows in population], dtype=np.int8)
-	im.set_data(new_im)
-	plt.pause(0.1)
+
+    new_im = np.array([[255 if val == -1 else 1 for val in rows] for rows in population], dtype=np.int8)
+    im.set_data(new_im)
+    plt.pause(0.1)
 
 def test_ising():
 	'''
@@ -186,22 +179,29 @@ def test_ising():
 	print("Testing ising model calculations")
 	population = -np.ones((3, 3))
 	assert(calculate_agreement(population,1,1)==4), "Test 1"
+
 	population[1, 1] = 1.
 	assert(calculate_agreement(population,1,1)==-4), "Test 2"
+
 	population[0, 1] = 1.
 	assert(calculate_agreement(population,1,1)==-2), "Test 3"
+
 	population[1, 0] = 1.
 	assert(calculate_agreement(population,1,1)==0), "Test 4"
+
 	population[2, 1] = 1.
 	assert(calculate_agreement(population,1,1)==2), "Test 5"
+
 	population[1, 2] = 1.
 	assert(calculate_agreement(population,1,1)==4), "Test 6"
+
 	"Testing external pull"
 	population = -np.ones((3, 3))
 	assert(calculate_agreement(population,1,1,1)==3), "Test 7"
 	assert(calculate_agreement(population,1,1,-1)==5), "Test 8"
-	assert(calculate_agreement(population,1,1,10)==14), "Test 9"
-	assert(calculate_agreement(population,1,1,-10)==-6), "Test 10"
+	assert(calculate_agreement(population,1,1,10)==-6), "Test 9"
+	assert(calculate_agreement(population,1,1, -10)==14), "Test 10"
+
 	print("Tests passed")
 
 
@@ -228,11 +228,9 @@ This section contains code for the Defuant Model - task 2 in the assignment
 '''
 
 def defuant_main():
-	print("")
 	#Your code for task 2 goes here
 
 def test_defuant():
-	print("")
 	#Your code for task 2 goes here
 
 
@@ -243,7 +241,6 @@ This section contains code for the main function- you should write some code for
 '''
 
 def main():
-	print("")
 	#You should write some code for handling flags here
 
 if __name__=="__main__":
